5033750edf9beacc804a7168ff91dc5c72798d17
Zhenqian Xu <axnshy@gmail.com>
2018-05-29 10:22:50 +0800

    Update minheap.py
    fix bugs

--- python/minheap.py
+++ python/minheap.py
@@ -1,20 +1,22 @@
 def _softup(x, pos):
     lt = len(x)
-    left = 2 * pos
-    right = 2 * pos + 1
+    if pos >= lt:
+        return
+    left = 2 * pos + 1
+    right = 2 * pos + 2
     smallest = None
     if isinstance(x[pos], tuple):
         if left < lt and x[left][0] < x[pos][0]:
-            smallest = pos
-        else:
             smallest = left
+        else:
+            smallest = pos
         if right < lt and x[right][0] < x[smallest][0]:
             smallest = right
     else:
         if left < lt and x[left] < x[pos]:
-            smallest = pos
-        else:
             smallest = left
+        else:
+            smallest = pos
         if right < lt and x[right] < x[smallest]:
             smallest = right
     if smallest != pos:
@@ -25,10 +27,8 @@ def _softup(x, pos):
 
 
 def heapify(x):
-    for i in reversed(len(x)/2):
-        if i == 0:
-            return
-        _softup(x, i + 1)
+    for i in reversed(range(len(x)//2)):
+        _softup(x, i)
 
 
 def pop(x):
@@ -38,11 +38,11 @@ def pop(x):
     temp = x[-1]
     if r == temp:
         x.pop(0)
-        return r.val, r
+        return r[0], r[1]
     x[0] = temp
-    x.pop(-1)
-    _softup(x, 1)
-    return r.val, r
+    x.pop(len(x) - 1)
+    _softup(x, 0)
+    return r[0], r[1]
 
 
 def push(x, item):
@@ -50,10 +50,10 @@ def push(x, item):
     lt = len(x)
     smallest = lt
     while lt // 2 > 0:
-        if x[lt // 2][0] < x[smallest][0]:
-            temp = x[lt // 2]
-            x[lt // 2] = x[smallest]
-            x[smallest] = temp
+        if x[(lt // 2)-1][0] > x[smallest-1][0]:
+            temp = x[(lt // 2)-1]
+            x[(lt // 2)-1] = x[smallest-1]
+            x[smallest-1] = temp
             smallest = lt // 2
         else:
             break
